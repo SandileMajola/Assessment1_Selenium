d94cccb06a254db1bfff5a2cc688b82ae01ee4ec
Sandile Majola <35298568+SandileMajola@users.noreply.github.com>
2020-10-06 05:27:27 +0200

    Add files via upload

---
+++ GlobalKinetic/GlobalKinetic/Browser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlobalKinetic
+{
+    public enum Browser
+    {
+        Chrome,
+        Firefox,
+        IE
+    }
+}
---
+++ GlobalKinetic/GlobalKinetic/Core.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Text;
+
+namespace GlobalKinetic
+{
+    public class Core
+    {
+        public IWebDriver _driver;
+
+        //Change this to run on another browser
+        public Browser Driver = Browser.Chrome;
+
+        public Core()
+        {
+            if (Driver == Browser.Chrome)
+            {
+                _driver = new ChromeDriver(@"C:\chromedriver");
+            }
+            else if (Driver == Browser.Firefox)
+            {
+                _driver = new FirefoxDriver();
+            }
+            else
+            {
+                _driver = new ChromeDriver();
+            }
+
+            _driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+
+
+
+    }
+}
